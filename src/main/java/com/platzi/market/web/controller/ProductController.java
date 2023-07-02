3a6b5492c4374c9658ee4831b57f363b0b6e2324
Brayan Rodallega <thebrayan1a1@outlook.com>
2023-07-01 23:53:14 -0500

    :sparkles: [ADD] Product Controller

---
+++ src/main/java/com/platzi/market/web/controller/ProductController.java
@@ -0,0 +1,43 @@
+package com.platzi.market.web.controller;
+
+import com.platzi.market.domain.Product;
+import com.platzi.market.domain.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/all")
+    public List<Product> getAll() {
+        return productService.getAll();
+    }
+
+    @GetMapping("/products/{productId}")
+    public Optional<Product> getProduct(@PathVariable("productId") int productId) {
+        return productService.getProduct(productId);
+    }
+
+    @GetMapping("/category/{categoryId}")
+    public Optional<List<Product>> getByCategory(@PathVariable("categoryId") int categoryId) {
+        return productService.getByCategory(categoryId);
+    }
+
+    @PostMapping("/save")
+    public Product save(@RequestBody Product product) {
+        return productService.save(product);
+    }
+
+    @DeleteMapping("/delete/{productId}")
+    public boolean delete(@PathVariable("productId") int productId) {
+        return productService.delete(productId);
+    }
+
+}
