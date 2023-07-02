3ec6878077fa13ecccbca7c328571e89e1ecfdaf
Brayan Rodallega <thebrayan1a1@outlook.com>
2023-07-02 11:18:54 -0500

    :sparkles: [ADD] Purchase: Entity, Mapper, Respository, Service and Controller

--- src/main/java/com/platzi/market/persistence/entity/Compra.java
+++ src/main/java/com/platzi/market/persistence/entity/Compra.java
@@ -29,7 +29,7 @@ public class Compra {
     @JoinColumn(name = "id_cliente", insertable = false, updatable = false)
     private Cliente cliente;
 
-    @OneToMany(mappedBy = "compra")
+    @OneToMany(mappedBy = "compra", cascade = {CascadeType.ALL})
     private List<ComprasProducto> productos;
 
 
